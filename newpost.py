ac07a74748c4e4d16b78272f9a18cb6934cea3df
Samson Yeung <samson_gh@onepatchdown.net>
2023-10-17 13:33:28 -0700

    initial commit of new post generator.

---
+++ newpost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import datetime
+import shutil
+
+filename_date = datetime.datetime.now().strftime('%Y-%m-%d')
+full_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%m:%d PST')
+
+title = input("Title with spaces")
+
+hyphened_title = '-'.join(title.split())
+
+with open('_posts/TEMPLATE.markdown', 'r') as file:
+    lines = file.readlines()
+
+for index, line in enumerate(lines):
+    if line.startswith('date:'):
+        lines[index] = 'date: {}\n'.format(full_date)
+    elif line.startswith('title:'):
+        lines[index] = 'title: {}\n'.format(title)
+
+out_file_name = '_posts/{}-{}.markdown'.format(filename_date, hyphened_title)
+with open(out_file_name, 'w') as file:
+        file.writelines(lines)
+
+print('wrote {}'.format(out_file_name))
